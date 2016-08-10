ec67c53338acaa87f8aa6657e3820d1c041d6063
Martin Janiczek <martin@janiczek.cz>
2016-08-10 09:08:45 +0200

    Work with 0.17, clarify directive attrs in doc
    There was also "ports-in" attr in the documentation which wasn't used in code so I revamped the documentation.

--- angular-elm.js
+++ angular-elm.js
@@ -2,49 +2,45 @@
 
     /**
      * Usage:
-     *  ## Elm in plain JavaScript
-     *  <script>
-     *   var msg = 'Hello world!';
-     *   // For fullscreen mode
-     *   var elm = Elm.fullscreen(Elm.ModuleName, { initMesage: msg})
-     *   // For embedded mode
-     *   //var elm = Elm.embed(divElement, Elm.ModuleName, { initMesage: msg})
-     *   // For worker mode
-     *   //var elm = Elm.embed(Elm.ModuleName, { initMesage: msg})
-     *   elm.ports.messageIn.send('hello')
-     *   elm.ports.messageOut.subscribe(function (msg) {
-     *      console.log(msg);
-     *   });
-     *   </script>
      *
-     *   ## Elm in AngularJS
      *   For fullscreen mode
-     *     <body elm module="ModuleName" ports-in="{ initMessage: msg }">
+     *     <body elm module="ModuleName">
+     *
      *   For embedded mode
-     *     <div elm module="ModuleName" ports-in="{ initMessage: msg }">
+     *     <div elm module="ModuleName">
+     *
      *   For worker mode
-     *   <elm module="ModuleName" ports-in="{ initMessage: msg }"> 
+     *     <elm module="ModuleName">
      *
-     *   <script>
-     *   // ...
-     *   $scope.msg = 'Hello world!';
-     *   // auto send when messageIn is change
-     *   $scope.messageIn = 'hello';
+     *   ----
      *
-     *   // auto subscribe/unsubscribe when messageOut is change
-     *   $scope.messageOut = function (msg) {
-     *      console.log(msg);
-     *   }
-     *   // ...
-     *   </script>
+     *   Flags (for Html.App.programWithFlags):
+     *     <div elm module="ModuleName" flags='{myValue: "Hello init!"}'>
+     *
+     *   ----
+     *
+     *   Ports prefix ("namespacing"):
+     *     <div elm module="ModuleName" ports="prefix">
+     *   Then instead of $scope.myPort you use $scope.prefix.myPort .
+     *
+     *   ----
+     *
+     *   Ports usage:
+     *
+     *   Sending from JS: (if you have Elm port 'fromJsToElm')
+     *     $scope.fromJsToElm = 'This gets automatically sent to Elm!';
+     *
+     *   Subscribing: (if you have Elm port 'fromElmToJs')
+     *     $scope.fromElmToJs = function (msg) {
+     *       console.log(msg);
+     *     }
      */
 
     ng.module('Elm', [])
         .directive('elm', function ($parse, $timeout) {
             function link(scope, element, attrs) {
-                var id         = attrs.id;
                 var target     = element[0];
-                var portsIn    = $parse(attrs.portsIn)(scope);
+                var flags      = $parse(attrs.flags)(scope);
                 var ports      = attrs.ports;
                 var moduleName = attrs.module;
                 var module     = Elm[moduleName];
@@ -53,18 +49,18 @@
 
                 if (target.nodeName === 'BODY') {
                     // <body elm module="" ...></body>
-                    elm = Elm.fullscreen(module, portsIn)
+                    elm = module.fullscreen(flags)
                 } else if (target.nodeName === 'ELM') {
                     // <elm module="" ...></elm>
-                    elm = Elm.worker(module, portsIn);
+                    elm = module.worker(flags);
                 } else if (target.nodeName === 'DIV') {
                     // <div elm module="" ...></div>
-                    elm = Elm.embed(module, target, portsIn );
+                    elm = module.embed(target, flags);
                 }
 
 
                 if (elm) {
-                    portsPrefix = ports? ports + '.' : '';
+                    var portsPrefix = ports ? ports + '.' : '';
                     ng.forEach (elm.ports, function(port, name) {
                         if (port.send) {
                             scope.$watch(portsPrefix + name, function(value) {
@@ -87,14 +83,14 @@
                                         $timeout(function () {
                                             newFn(value);
                                         });
-                                    }
+                                    };
                                     port.subscribe(adapterFn);
                                 }
                             });
                         }
                     });
                 }
-            };
+            }
 
             return {
                 restrict: 'EA',
@@ -102,4 +98,3 @@
             };
         });
 })(angular, Elm);
-
