79c575d1d4f8db16cf8526a7bedb244b65ae56ee
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-01-16 12:36:46 -0500

    Code style consistency. (Manually enforced for now until I get eslint in there)

--- angularjs-ng-elm.js
+++ angularjs-ng-elm.js
@@ -61,15 +61,18 @@
 
     function getElmModule(elmObject, moduleName) {
       var module = diveIntoObject(elmObject, moduleName)
+      var availableModules, error
+
       if (!module || !isValidModule(module)) {
-        var availableModules = getElmModuleNames(elmObject);
-        var error = 'Could not find an Elm module named "' + moduleName +
+        availableModules = getElmModuleNames(elmObject)
+        error = 'Could not find an Elm module named "' + moduleName +
             '". I found these Elm modules instead: ' + availableModules.join(', ')
+
         throw new Error(error)
       }
       return module
     }
-    
+
     function isValidModule(elmObject) {
       return typeof elmObject.init === 'function'
     }
@@ -78,14 +81,14 @@
       // If you have an Elm module, the JS Elm object will represent that module as one or more objects,
       // with one object nested into the next per dot in the module name. Each module has an init function,
       // so whenever we find such a function, we know we have found the end of the module name.
-      return Object.keys(elmObject).map(function (moduleName) {
+      return Object.keys(elmObject).map(function(moduleName) {
         if (isValidModule(elmObject[moduleName])) {
           return [moduleName]
-        } else {
-          return getElmModuleNames(elmObject[moduleName]).map(function (subModuleName) {
-            return moduleName + '.' + subModuleName
-          })
         }
+
+        return getElmModuleNames(elmObject[moduleName]).map(function(subModuleName) {
+          return moduleName + '.' + subModuleName
+        })
       })
     }
 
